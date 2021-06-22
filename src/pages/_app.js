35860838b10bda2883373f52dcde71fc064fdb0a
Luca Galeazzi <jokgitti@gmail.com>
2021-06-22 18:51:26 +0200

    feat: added layout on app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,13 @@ import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <main className="bg-gradient-to-br from-purple-400 via-pink-500 to-red-500 min-h-screen">
+            <div className="container mx-auto px-4 pt-8 pb-8">
+                <Component {...pageProps} />
+            </div>
+        </main>
+    );
 }
 
 export default MyApp;
